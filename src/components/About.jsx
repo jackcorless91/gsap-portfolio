366e0f836e7cf6f0adce97c3413230903877728d
jackcorless91 <146907668+jackcorless91@users.noreply.github.com>
2025-10-12 07:55:22 +1100

    index content about page, rows  and heading

---
+++ src/components/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function About() {
+  return (
+    <section className="min-h-[calc(100vh-4.5rem)] md:min-h-[calc(100vh-5.5rem)] text-white grid grid-rows-3 gap-1 pt-8">
+      <div className="text-[12em] w-screen tracking-tight uppercase text-center">Software developer</div>
+      <div>name and desc</div>
+      <div>other ?</div>
+    </section>
+
+  );
+}
+
+export default About;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,18 @@
 import Menu from "@/components/Menu.jsx";
 import React from "react";
+import About from "@/components/About.jsx";
 
 function Home() {
   return (
       <main>
-
+        <div className="min-h-screen">
           <h1 className="page-title">
             index <sup>(01)</sup>
           </h1>
+        </div>
 
-        </main>
+        <About />
+      </main>
   );
 }
 
