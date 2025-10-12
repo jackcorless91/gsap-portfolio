1e139a1082561fb683eba67bc50aa1ca39b695fc
jackcorless91 <146907668+jackcorless91@users.noreply.github.com>
2025-10-13 07:29:28 +1100

    home page content complete, add animations and photos

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,22 +4,27 @@ function About() {
   return (
     <section
       className="min-h-[calc(100vh-4.5rem)] md:min-h-[calc(100vh-5.5rem)] text-white grid grid-rows-3 gap-1 pt-8">
-      <div className="text-[12vw] w-screen tracking-tight uppercase text-center">Software developer</div>
+      <div className="text-[11.5vw] w-screen tracking-tight uppercase text-center font-medium">Software developer</div>
       {/* add sm screen for font, wrap to second line ? */}
       <div className="flex justify-center items-center">
         <div className="w-80 h-full grid grid-rows-[25%_50%_25%]">
-          <div className="flex justify-center items-center flex-col tracking-tight text">
+          <div className="flex justify-end text-center flex-col tracking-tight uppercase text-6xl">
             im jack
           </div>
-          <div className="flex justify-center items-center">
+          <div className="flex justify-center items-center text-center text-[]">
             a software developer from Melbourne specialising in Javascript/typescript, with experience in python & golang.
           </div>
-          <div className="flex justify-center items-center">25%
-          {/* connect and view my work button */}
+          <div className="flex justify-between items-center">
+            <button className="uppercase">view my work</button>
+            <button className="uppercase">get in touch</button>
           </div>
 
         </div>
-        <div className="border-white border-solid outline w-80 h-full">photo container</div>
+        <div className="w-80 h-full flex justify-center items-center">
+          <div className="bg-white h-[85%] w-[85%] flex justify-center items-center">
+            <div className="text-black">photo</div>
+          </div>
+        </div>
       </div>
       <div>other ?</div>
     </section>
