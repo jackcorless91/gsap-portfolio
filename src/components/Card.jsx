906a139d03adc91bab94f0062b65fc26e157de56
jackcorless91 <146907668+jackcorless91@users.noreply.github.com>
2025-10-16 08:33:18 +1100

    responsive porject acrd complete on all sizes

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,28 +6,28 @@ function Card({ title, technologies, description, src, github, url, color }) {
   return (
     <div className="min-h-screen flex justify-center items-center sticky top-0">
       <div
-        className="w-4xl h-100 relative top-[-10%] rounded-2xl grid grid-rows-[25%_75%]"
+        className="w-4xl max-w-[80%] h-100 relative top-[-10%] rounded-2xl grid grid-rows-[25%_75%] max-sm:grid-rows-[30%_70%]"
         style={{ backgroundColor: color }}
       >
         {/* responsive updates to be made: md screens shrink scaling of card content
             small screens remove image and use same title size */}
         {/* title container */}
         <div className="flex flex-col items-center justify-center">
-          <h2 className="font-semibold tracking-tight uppercase text-4xl">{ title }</h2>
-          <h3 className="font-medium">{ technologies }</h3>
+          <h2 className="font-semibold tracking-tight uppercase text-center text-4xl max-sm:text-3xl">{ title }</h2>
+          <h3 className="font-medium text-center">{ technologies }</h3>
         </div>
         {/* content container */}
-        <div className="grid grid-cols-2">
-          <div className="grid grid-rows-[60%_40%]">
+        <div className="grid grid-cols-2 max-sm:grid-cols-1">
+          <div className="grid grid-rows-[60%_40%] max-sm:grid-rows-[70%_30%]">
             <div className="flex justify-center items-center">
               <div className="flex w-[80%] h-full">
-                <p>{ description }</p>
+                <p className="description max-h-full overflow-y-scroll">{ description }</p>
               </div>
             </div>
             <div className="uppercase font-medium tracking-tight h-full w-full flex justify-center items-center">
               <div className="ctn flex w-[80%] items-center justify-between">
                 <a
-                  href={github}
+                  href={ github }
                   target="_blank"
                   rel="noopener noreferrer"
                   className="m-6 group relative w-max cursor-pointer"
@@ -36,7 +36,7 @@ function Card({ title, technologies, description, src, github, url, color }) {
                   <span className="absolute -bottom-1 left-0 w-0 h-0.5 bg-black transition-all group-hover:w-full"></span>
                 </a>
                 <a
-                  href={url}
+                  href={ url }
                   target="_blank"
                   rel="noopener noreferrer"
                   className="m-6 group relative w-max cursor-pointer"
@@ -47,10 +47,10 @@ function Card({ title, technologies, description, src, github, url, color }) {
               </div>
             </div>
           </div>
-          <div className="flex items-start justify-center">
-            <div className="w-[80%] h-[90%]">
+          <div className="flex items-start justify-center max-sm:hidden">
+            <div className="w-[80%] h-[90%] rounded-2xl bg-black">
               <img
-                src={src}
+                src={ src }
                 alt="image"
               />
             </div>
