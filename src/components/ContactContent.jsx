d8f0e3f83706b9f5c0e869c98f02c5ae2d88daba
jackcorless91 <146907668+jackcorless91@users.noreply.github.com>
2025-10-19 18:20:27 +1100

    contact from basic

--- src/components/ContactContent.jsx
+++ src/components/ContactContent.jsx
@@ -2,19 +2,36 @@ import React from 'react';
 
 function ContactContent() {
   return (
-    <section className="min-h-[calc(100vh-4.5rem)] md:min-h-[calc(100vh-5.5rem)] flex items-center justify-center">
-      <div className="text-white">
-        <div className="text-7xl tracking-tight uppercase font-medium">Lets Connect.</div>
+    <section className="min-h-[calc(100vh-4.5rem)] md:min-h-[calc(100vh-5.5rem)] flex items-center justify-center ">
+      <div className="text-white flex flex-col gap-14  max-h-full max-sm:gap-6">
+        <div className="text-7xl tracking-tight uppercase font-medium max-sm:text-5xl  pt-[30vh]">Lets Connect.</div>
         <div className="form-container">
-          <form action="">
-            <label htmlFor="name"></label>
-            <input type="text" id="name" name="name" />
-            <label htmlFor="phone"></label>
-            <input type="text" id="phone" />
-            <label htmlFor="email"></label>
-            <input type="text" id="email" />
-            <label htmlFor="message"></label>
-            <input type="text" id="message" />
+          <form action="" className="flex flex-col tracking-tight uppercase font-medium gap-2 w-full">
+            <label htmlFor="name">Name*</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              className="bg-transparent border-0 border-b-[0.5px] border-white text-white placeholder-white focus:outline-none focus:ring-0"
+            />
+            <label htmlFor="phone">Phone*</label>
+            <input
+              type="text"
+              id="phone"
+              className="bg-transparent border-0 border-b-[0.5px] border-white text-white placeholder-white focus:outline-none focus:ring-0 "
+            />
+            <label htmlFor="email">Email*</label>
+            <input
+              type="text"
+              id="email"
+              className="bg-transparent border-0 border-b-[0.5px] border-white text-white placeholder-white focus:outline-none focus:ring-0"
+            />
+            <label htmlFor="message">Message*</label>
+            <input
+              type="text"
+              id="message"
+              className="bg-transparent border-0 border-b-[0.5px] border-white text-white placeholder-white focus:outline-none focus:ring-0"
+            />
           </form>
         </div>
       </div>
